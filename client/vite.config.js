86849a315d62416e1d8acdc7418bd2586805dc9a
rolix202 <williamsroland147@gmail.com>
2024-11-15 11:26:00 +0100

    update the proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
   server: {
     proxy: {
-      '/api' : {
-        target: 'http://localhost:5000/api', changeOrigin: true,
+      '/api': {
+        target: process.env.VITE_BACKEND_ENDPOINT || 'http://localhost:5000',
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
-})
+      },
+    },
+  },
+}));

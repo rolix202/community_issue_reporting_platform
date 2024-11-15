e75c59caeb90fc7ea7871191924de7b7d22c3288
rolix202 <williamsroland147@gmail.com>
2024-11-15 11:38:24 +0100

    fixed the proxy issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const backendEndpoint = process.env.VITE_BACKEND_ENDPOINT || 'http://localhost:5000';
+
 export default defineConfig(() => ({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_ENDPOINT || 'http://localhost:5000',
+        target: `${backendEndpoint}/api`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
